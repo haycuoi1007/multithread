5b9558eb29b41e890bb3423737b6ea3e058899be
haycuoi1007 <belv.th@gmail.com>
2020-02-15 23:23:24 +0700

    modified main function

--- main.cpp
+++ main.cpp
@@ -9,19 +9,8 @@ void *InLoiChao(void *threadid)
    cout << "Hello World! Thread ID la " << threadid << endl;
    pthread_exit(NULL);
 
-}int main ()
+}
+int main ()
 {
-   pthread_t threads[SO_THREAD];
-   int rc;
-   int i;
-   for( i=0; i < SO_THREAD; i++ ){
-      cout << "\nmain() : dang tao thread! " << i << endl;
-      rc = pthread_create(&threads[i], NULL,
-                          InLoiChao, (int *)i);
-      if (rc){
-         cout << "\nError: Khong the tao thread!" << rc << endl;
-         exit(-1);
-      }
-   }
-   pthread_exit(NULL);
+
 }
